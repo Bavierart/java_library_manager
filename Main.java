ba4eef1993d3d644dda68a211a613c18d2d42c97
Felipe <bavierart@gmail.com>
2025-11-08 20:23:20 -0300

    Mudança docs Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
-import model.User;
 import view.MainView;
 import view.factory.*;
 
+/**
+ * Aplicativo de gerenciamento de biblioteca,
+ * argumento --gui para interface visual
+ * sem argumentos para interface textual
+ * **/
 public class Main {
     public static void main(String[] args) {
         boolean gui = args.length > 0 && "gui".equalsIgnoreCase(args[0]);
